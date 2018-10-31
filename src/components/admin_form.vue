34d58bc893b76d9d481df23b9f40000ef929caef
Maisara Abou-Rady <maisara.m.abourady@gmail.com>
2018-10-31 11:38:19 +0000

    Removing Vuex CLI Scaffold as it's not working properly

--- src/components/admin_form.vue
+++ src/components/admin_form.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <admin_form></admin_form>
+
   </div>
 </template>
 
 <script>
-
-import AdminForm from './components/admin_form.vue'
-
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Maisara'
     }
-  },
-
-  components: {
-
-    admin_form: AdminForm
-
   }
 }
 </script>
@@ -40,17 +30,4 @@ h1, h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
